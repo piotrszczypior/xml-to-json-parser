edd04d7b22a0ce153c47977e865b148fb9401a89
Piotr Szczypior <piotrekszczypior1@gmail.com>
2025-04-13 16:44:23 +0200

    init react vite app with typescript and sass

---
+++ parser-client/src/routes.ts
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App.tsx";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: App,
+        children: [
+            {
+                path: "parse",
+            }
+        ]
+    }
+])
+
